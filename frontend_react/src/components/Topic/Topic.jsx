8fec3953b47ee56927f1b4c844149a62678212fc
Nikita Obergan <Obergannikita@gmail.com>
2023-10-09 12:54:09 -0400

    Add topic page

---
+++ frontend_react/src/components/Topic/Topic.jsx
@@ -0,0 +1,94 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+
+const Topic = ({ topicItem }) => {
+  console.log(topicItem);
+
+  const [user, token] = useAuth();
+  const [isEditing, setIsEditing] = useState(false);
+  const [text, setText] = useState();
+  const [title, setTitle] = useState();
+  console.log(user);
+
+  var checkProdileIsAuthorizedUser = user.id === topicItem.authorOfTopic.id;
+
+  const handleUpdateButton = async (e) => {
+    e.preventDefault();
+    setIsEditing(!isEditing);
+    setText(topicItem.text);
+    setTitle(topicItem.title);
+  };
+
+  const updatedTopic = {
+    title,
+    text,
+  };
+
+  const handleUpdateTopic = async (e) => {
+    try {
+      e.preventDefault();
+      const response = await axios.put(
+        `https://localhost:5001/api/topic/${topicItem.topicId}`,
+        updatedTopic,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      setIsEditing(!isEditing);
+    } catch (error) {
+      console.warn("Error in Topic Page , Update Button", error);
+    }
+  };
+
+  //date format
+  const shortDateFormat = dayjs(topicItem.timePosted).format("MM/DD/YYYY");
+
+  return (
+    <div>
+      <div>
+        <div>Author: {topicItem.authorOfTopic.userName}</div>
+        <div>PublishedDate: {shortDateFormat}</div>
+        <div>Likes: {topicItem.likes}</div>
+      </div>
+
+      {isEditing ? (
+        <div>
+          <form onSubmit={handleUpdateTopic}>
+            <label>Title</label>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            ></input>
+            <br />
+            <br />
+            <label>Text</label>
+            <textarea
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            ></textarea>
+
+            <button type="submit">Save</button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <div className="d-flex justify-content-center">{topicItem.title}</div>
+          <div>Text: {topicItem.text}</div>
+        </div>
+      )}
+
+      <div>
+        {checkProdileIsAuthorizedUser && !isEditing ? (
+          <button onClick={handleUpdateButton}>Update</button>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Topic;
--- frontend_react/src/pages/TopicPage/TopicPage.js
+++ frontend_react/src/pages/TopicPage/TopicPage.js
@@ -1,5 +1,34 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
+
+//components
+import Topic from "../../components/Topic/Topic";
+
 const TopicPage = () => {
-  return <div></div>;
+  const [user, token] = useAuth();
+  const { topicId } = useParams();
+  const [topicItem, setTopicItem] = useState(null);
+  const [topicReviews, setTopicReviews] = useState(null);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    fetchTopic();
+  }, [topicItem]);
+
+  const fetchTopic = async () => {
+    try {
+      let response = await axios.get(
+        `https://localhost:5001/api/topic/${topicId}`
+      );
+      setTopicItem(response.data);
+    } catch (error) {
+      console.log("Error in fetchTopic by id ", error);
+    }
+  };
+
+  return <div>{topicItem && <Topic topicItem={topicItem} />}</div>;
 };
 
 export default TopicPage;
