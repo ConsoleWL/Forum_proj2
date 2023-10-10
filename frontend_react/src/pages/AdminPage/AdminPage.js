68317b7c41d35eec52ea95e24bdb4c12b0b8cb80
Nikita Obergan <Obergannikita@gmail.com>
2023-10-10 17:05:01 -0400

    Add charts

--- frontend_react/src/pages/AdminPage/AdminPage.js
+++ frontend_react/src/pages/AdminPage/AdminPage.js
@@ -1,5 +1,26 @@
+import { Chart } from "react-google-charts";
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
+];
+export const options = {
+  title: "My Daily Activities",
+};
+
 const AdminPage = () => {
-  return <div>Admin Page</div>;
+  return (
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
+  );
 };
 
 export default AdminPage;
